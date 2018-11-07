adec4ffc6395a58894ae60e0bf085882665e0d00
dgkim <dgkim@dgkim-PC>
2018-11-07 15:15:39 +0900

    a

--- node-Ex1/ch3/3.6/writeFile.js
+++ node-Ex1/ch3/3.6/writeFile.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
-fs.writeFile('./writeme.txt', '글이 입력됩니다', (err) => {
+fs.writeFile('ch3/3.6/writeme.txt', '글이 입력됩니다', (err) => {
   if (err) {
     throw err;
   }
-  fs.readFile('./writeme.txt', (err, data) => {
+  fs.readFile('ch3/3.6/writeme.txt', (err, data) => {
     if (err) {
       throw err;
     }
