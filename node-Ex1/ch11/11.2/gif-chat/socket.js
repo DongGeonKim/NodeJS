c169d5f6f44efb674390f600146a2521a873d6d7
dgkim <dgkim@58.228.205.28>
2018-11-11 22:16:15 +0900

    ㅁㅁㅁ

--- node-Ex1/ch11/11.2/gif-chat/socket.js
+++ node-Ex1/ch11/11.2/gif-chat/socket.js
@@ -7,7 +7,7 @@ module.exports = (server) => {
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     console.log('새로운 클라이언트 접속', ip);
     ws.on('message', (message) => {
-      console.log(message);
+      console.log("socket.js : " + message);	//클라이언트로부터 메시지가 왔을때 발생
     });
     ws.on('error', (error) => {
       console.error(error);
@@ -18,7 +18,7 @@ module.exports = (server) => {
     });
     const interval = setInterval(() => {
       if (ws.readyState === ws.OPEN) {
-        ws.send('서버에서 클라이언트로 메시지를 보냅니다.');
+        ws.send('서버에서 클라이언트로 메시지를 보냅니다.');	//클라이언트로 보내는 문구
       }
     }, 3000);
     ws.interval = interval;
